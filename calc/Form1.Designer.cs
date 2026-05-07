ff4c8fbc76e7771410f9cc1a6283fbaaa430615c
belladonna.giacomo <belladonna.giacomo@HP-002>
2026-05-07 10:41:34 +0200

    mod

--- calc/Form1.Designer.cs
+++ calc/Form1.Designer.cs
@@ -28,18 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            num1txt = new TextBox();
+            label2 = new Label();
+            num2txt = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            lblris = new Label();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ButtonFace;
+            label1.Location = new Point(159, 133);
+            label1.Name = "label1";
+            label1.Size = new Size(68, 20);
+            label1.TabIndex = 0;
+            label1.Text = "numero1";
+            // 
+            // num1txt
+            // 
+            num1txt.Location = new Point(159, 156);
+            num1txt.Name = "num1txt";
+            num1txt.Size = new Size(215, 27);
+            num1txt.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(159, 199);
+            label2.Name = "label2";
+            label2.Size = new Size(68, 20);
+            label2.TabIndex = 2;
+            label2.Text = "numero2";
+            // 
+            // num2txt
+            // 
+            num2txt.Location = new Point(159, 222);
+            num2txt.Name = "num2txt";
+            num2txt.Size = new Size(215, 27);
+            num2txt.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(673, 180);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 9;
+            button1.Text = "+";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(826, 180);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 10;
+            button2.Text = "-";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(673, 243);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 11;
+            button3.Text = "*";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(826, 243);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 12;
+            button4.Text = "/";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // lblris
+            // 
+            lblris.AutoSize = true;
+            lblris.Location = new Point(776, 345);
+            lblris.Name = "lblris";
+            lblris.Size = new Size(63, 20);
+            lblris.TabIndex = 13;
+            lblris.Text = "risultato";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1195, 635);
+            Controls.Add(lblris);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(num2txt);
+            Controls.Add(label2);
+            Controls.Add(num1txt);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox num1txt;
+        private Label label2;
+        private TextBox num2txt;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label lblris;
     }
 }
--- calc/Form1.cs
+++ calc/Form1.cs
@@ -6,5 +6,48 @@ namespace calc
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1, n2, somma;
+            n1 = Convert.ToInt32(num1txt.Text);
+            n2 = Convert.ToInt32(num2txt.Text);
+            somma = n1 + n2;
+            lblris.Text = "Risultato: " + somma;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int n1, n2, sottrazione;
+            n1 = Convert.ToInt32(num1txt.Text);
+            n2 = Convert.ToInt32(num2txt.Text);
+            sottrazione = n1 - n2;
+            lblris.Text = "Risultato: " + sottrazione;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int n1, n2, molt;
+            n1 = Convert.ToInt32(num1txt.Text);
+            n2 = Convert.ToInt32(num2txt.Text);
+            molt = n1 * n2;
+            lblris.Text = "Risultato: " + molt;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int n1, n2, div;
+            n1 = Convert.ToInt32(num1txt.Text);
+            n2 = Convert.ToInt32(num2txt.Text);
+            if (n2 == 0)
+            {
+                lblris.Text = "impossibile ";
+            }
+            else
+            {
+                div = n1 / n2;
+                lblris.Text = "Risultato: " + div;
+            }
+        }
     }
 }
